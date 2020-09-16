211f3d697784910dff5b5e086c6169552afbcc0e
ndev <napodevesa@gmail.com>
2020-09-16 23:16:31 +0200

    changes PAC1

--- PAC1_20201_IB/PAC1Ex1/src/PAC1Ex1.java
+++ PAC1_20201_IB/PAC1Ex1/src/PAC1Ex1.java
@@ -1,22 +1,39 @@
-import java.util.Scanner;
 
 public class PAC1Ex1 {
-	
-	static Scanner lector = new Scanner (System.in);
-    static double fahrenheit;
 
 	public static void main(String[] args) {
-		// 96.8 °F son 36 °C // 
+		// 96.8 °F son 36 °C //
+		double fahrenheit = 96.8, celsius;
 		
-		System.out.println("Introduce ºF");
-		fahrenheit = lector.nextDouble();
-
-		double celsius = f2c(fahrenheit);
+		celsius = f2c(fahrenheit) - 5;
 		
-		System.out.print("El resultado es el siguiente: " + celsius + "°C = "+ fahrenheit +"°F");
+		System.out.print(celsius + "°C = "+ fahrenheit +"°F");
 	}
 	
 	public static double f2c(double f) {
 		return (f - 32) * 5 / 9;		
 	}
 }
+
+/*
+import java.util.Scanner;
+
+public class PAC1Ex1 {
+
+	public static void main(String[] args) {
+		
+		try (Scanner input = new Scanner(System.in)) {
+			System.out.print("Input a degree in Fahrenheit: ");
+			double fahrenheit = input.nextDouble();
+
+			double  celsius = f2c(fahrenheit);
+			System.out.println(fahrenheit + " degree Fahrenheit is equal to " 
+			+ celsius + " in Celsius");
+		}
+	}
+	
+	public static double f2c(double f) {
+		return ((f - 32) * 5 / 9);		
+	}
+}
+*/
