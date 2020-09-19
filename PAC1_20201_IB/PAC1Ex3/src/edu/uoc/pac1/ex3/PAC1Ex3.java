7419f996e742b6be6c20c4084a1780a1c69db0de
ndev <napodevesa@gmail.com>
2020-09-19 12:12:18 +0200

    lengthSubVector(vector, even) OK

--- PAC1_20201_IB/PAC1Ex3/src/edu/uoc/pac1/ex3/PAC1Ex3.java
+++ PAC1_20201_IB/PAC1Ex3/src/edu/uoc/pac1/ex3/PAC1Ex3.java
@@ -6,71 +6,68 @@ public class PAC1Ex3 {
 	public static void main(String[] args) {
 		int[] vector = {2,3,4,5,6};
 		
-		System.out.println(Arrays.toString(subVector(vector)));
+		boolean even= false;
+		lengthSubVector(vector, even);
+		
+		System.out.println(lengthSubVector(vector, even));
+		
+		//System.out.println(Arrays.toString(subVector(vector)));
 	}
 	
 	public static int[] subVector(int[] vector) {	
 		
-		boolean even= true;
-		int[] newVector = new int[lengthSubVector(vector,even)];
-		int contador=0;
+		//boolean even= true;
+		int[] newVector = new int[5];
 		
 		if (vector.length == 0) {
 			System.out.println ("[ERROR] Vector is null");
-			
 		}
 		
-		
-		if (vector.length != 0) {
-			
-				if (vector[0]%2==0) {
-					for (int i = 0; i < vector.length; i++) {
-						if (vector[i]%2==0) {
-							newVector[i]=vector[i];
-						}
-					}
-					contador+=1;
-				}
+		while (vector.length > 0) {
+					if (vector[0]%2==0) {
+							for (int i = 0; i < vector.length; i++) {
+								if (vector[i]%2==0) {
+									newVector[i]=vector[i];
+								}
+							}
+					
+					}else {
 				
-				if (vector[0]%2!=0) {
-					for (int i = 0; i < vector.length; i++) {
-						if (vector[i]%2!=0) {
-							newVector[i]=vector[i];
-						}
+						if (vector[0]%2!=0) {
+								for (int i = 0; i < vector.length; i++) {
+									if (vector[i]%2!=0) {
+										newVector[i]=vector[i];
+									}
+								}
+						}	
 					}
-					contador+=1;
-				}
-				return newVector;
-		}	
-}
+		}
+		return newVector;
+					
+	}	
 
 	
 	public static int lengthSubVector(int[] vector, boolean even) {
 		int contador = 0;
-		 
-        if (vector.length == 0) {
-        	
-			System.out.println ("[ERROR] Vector is null");
-			//return contador;
-		}else {
-			for (int i = 0; i < vector.length; i++) {
+
+		        if (vector.length == 0) {
+		        	
+					System.out.println ("[ERROR] Vector is null");
+					contador = 0;
 				
-				int x = vector[i];
-				
-				if (even) {
-					if (x%2 == 0) {
-						contador += 1;
+		        }else {
+					for (int i = 0; i < vector.length; i++) {
+							int x = vector[i];
+							
+							if (even && x%2 == 0) {
+									contador += 1;
+							}
+							if (even == false && x%2 != 0) {
+								contador += 1;
+							}
 					}
-				}else{
-					if (x%2 != 0) {
-						contador += 1;
-						}
-				
-				  }
-				
-			}
-		}
+		
+				}
 		return contador;
-	}
-	
-}
+	} 
+} 
