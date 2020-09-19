5978165d7c7a4ba7bd89b8e9f00a6d834a3a6ad4
ndev <napodevesa@gmail.com>
2020-09-19 15:56:37 +0200

    finish

--- PAC1_20201_IB/PAC1Ex3/src/edu/uoc/pac1/ex3/PAC1Ex3.java
+++ PAC1_20201_IB/PAC1Ex3/src/edu/uoc/pac1/ex3/PAC1Ex3.java
@@ -4,11 +4,8 @@ import java.util.Arrays;
 public class PAC1Ex3 {
 
 	public static void main(String[] args) {
+		
 		int[] vector = {2,3,4,5,6};
-		//boolean even= false;
-		//lengthSubVector(vector, even);
-		//System.out.println(lengthSubVector(vector, even));
-		//System.out.println(subVector(vector));
 		
 		System.out.println(Arrays.toString(subVector(vector)));
 	}
@@ -19,25 +16,34 @@ public class PAC1Ex3 {
 		boolean even = true;
 		int[] subVector = new int[lengthSubVector(vector, even)];
 		
+		if (vector == null) {
+			return vector;
+			
+		}
+		
 		if (vector.length == 0) {
 			System.out.println ("[ERROR] Vector is null");
+			return null;
 		}
 		
 		if (vector.length > 0) {
 			for (int i=0; i<vector.length; i++) {
 				
-				if (vector[0]%2==0 && vector[i]%2==0) {
-						subVector[j]=vector[i];
-						j++;
-				
-				}
+					if (vector[0]%2==0) {
+							if (vector[i]%2==0) {
+								subVector[j]=vector[i];
+								j++;
+						
+						    }
+					}
 			
-		
-				if (vector[0]%2!=0 && vector[i]%2!=0) {
-					subVector[j]=vector[i];
-					j++;
-			
-				}
+					if (vector[0]%2!=0) {
+						if (vector[i]%2!=0) {
+							subVector[j]=vector[i];
+							j++;
+					
+					    }
+					}
 			
 			}
 	}
@@ -48,24 +54,32 @@ public class PAC1Ex3 {
 	
 	public static int lengthSubVector(int[] vector, boolean even) {
 		int contador = 0;
-		        if (vector.length == 0) {
-		        	
-					System.out.println ("[ERROR] Vector is null");
-					contador = 0;
-				
-		        }else {
+		
+		if (vector == null) {
+			contador = 0;
+			System.out.println ("[ERROR] Vector is null!!");
+			
+		}
+		       
+		if (vector.length == 0 ) {
+			contador = 0 ;
+		 }
+		
+		if (vector.length > 0 && vector != null) {
 					for (int i = 0; i < vector.length; i++) {
 							int x = vector[i];
-							
-							if (even && x%2 == 0) {
+				
+							while (even == true) {
+									if (x%2 == 0);
 									contador += 1;
 							}
-							if (even == false && x%2 != 0) {
+							while (even == false) { 
+								if (x%2 != 0);
 								contador += 1;
 							}
 					}
 		
-				}
+		}
 		return contador;
 	} 
 } 
